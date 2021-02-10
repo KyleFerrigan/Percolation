304379af090082c6b203dd32be9b6a396a0ed51e
Kyle Ferrigan <kbferrigan@outlook.com>
2021-02-10 14:13:14 -0600

    Merged changes and fixed some brackets

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,18 +2,17 @@ public class Percolation {
 
     boolean[][] grid;//Array that holds grid of open or closed sites
 
-    public Percolation(int N){ //create N-by-N grid, with all sites blocked
+    public Percolation(int N) { //create N-by-N grid, with all sites blocked
         try {
             grid = new boolean[N][N];
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println("N cannot be less than or equal to 0");
         }
-            for (int i = 0; i<N; i++){
-                for (int j=0; j<N; j++){
-                    grid[i][j] = false; //False means blocked, all sites blocked with this command
-                }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = false; //False means blocked, all sites blocked with this command
             }
-
+        }
     }
 
     public void open(int i, int j){ //open site (row i, column j) if it is not open already
