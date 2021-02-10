226972808ce059a07981a55949c19712d53e2e01
Kyle Ferrigan <kbferrigan@outlook.com>
2021-02-10 14:03:47 -0600

    Added functionality to percolates() function

--- src/Percolation.java
+++ src/Percolation.java
@@ -21,15 +21,26 @@ public class Percolation {
 
     public boolean isFull(int i, int j){ //is site (row i, column j) full?
         //TODO check sites around the given site and find a route to the top, if unable to do so return false
+
         return false;//placeholder
     }
 
     public boolean percolates(){ //does the system percolate?
         //TODO check bottom sites for a site that is open, if so call isFull
-        return false; //placeholder
+        int size = grid.length;
+        for (int i = 0; i<size; i++) {
+            if (isOpen(size,i)){ //if bottom site is open
+                if(isFull(size,i)){ //if bottom site is full, this is more expensive so checking if its open first makes more sense
+                    return true; //if bottom site is full it means it will percolate, no more sites need to be tested.
+                }
+            }
+        }
+        return false; //if no full sites found this will execute
     }
 
     public static void main(String args[]){
+        Percolation p = new Percolation(20);
+        p.percolates();
         System.out.println("hello world");
     }
 }
