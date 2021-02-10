98acf0b6643357284995fc515d2c4501bb1babe9
pagalati <78822760+pagalati@users.noreply.github.com>
2021-02-10 17:20:57 -0600

    Update PercolationStats.java
    Started working on the constructor

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,25 +1,46 @@
-public class PercolationStats {
+public class PercolationStats 
+{
+    private double[] grid;
+    
+    public PercolationStats(int N, int T)
+    { // perform T independent experiments on an N-by-N grid
+        try {
+            grid = new double [T];
+            for(int i=0; i<T; i++) {
+                Percolation p = new Percolation(N);
 
-    public PercolationStats(int N, int T){ // perform T independent experiments on an N-by-N grid
-
+                }
+            }
+        }
+        catch (java.lang.IllegalArgumentException e) {
+            if (N<=0) {
+                System.out.println("N cannot be less than or equal to 0");
+            }
+            if (T<=0) {
+                System.out.println("T cannot be less than or equal to 0");
+            }
+        }
     }
 
-    public double mean(){ //sample mean of percolation threshold
-
+    public double mean()
+    { //sample mean of percolation threshold
         return -1; //placeholder
     }
 
-    public double stddev(){ //sample standard deviation of percolation threshold
+    public double stddev()
+    { //sample standard deviation of percolation threshold
 
         return -1;//placeholder
     }
 
-    public double confidenceLow(){ //low  endpoint of 95% confidence interval
+    public double confidenceLow()
+    { //low  endpoint of 95% confidence interval
 
         return -1;//placeholder
     }
 
-    public double confidenceHigh(){ //high endpoint of 95% confidence interval
+    public double confidenceHigh()
+    { //high endpoint of 95% confidence interval
 
         return -1; //placeholder
     }
