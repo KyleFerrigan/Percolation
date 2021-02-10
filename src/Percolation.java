1de8b858792b5b365c16d27c79088a5f7a64b42f
Kyle Ferrigan <kbferrigan@outlook.com>
2021-02-10 14:41:34 -0600

    isFull framework

--- src/Percolation.java
+++ src/Percolation.java
@@ -34,12 +34,41 @@ public class Percolation {
         }
     }
 
-    public boolean isFull(int i, int j){ //is site (row i, column j) full?
+    public boolean isFull(int i, int j) { //is site (row i, column j) full?
         try {
             //TODO check sites around the given site and find a route to the top, if unable to do so return false
+            int size = grid.length;
+            //check what edge we are on
+            if (j == size - 1) {//check if on the bottom row
+                if (i == size - 1) {//bottom right
 
-            return false;//placeholder
+                }
+                else if (i == 0) {//bottom left
+
+                }
+                else {// bottom somewhere in the middle aisles
+
+                }
+            }
+
+            else if (j == 0) { //top row
+                return isOpen(i, j);//if in top row always full if its open
+            }
+
+            else {//in the middle rows
+                if (i == 0) {//site is on left aisle
+
+                }
+                else if (i == size - 1) {//site is on right aisle
+
+                }
+                else {//site is not on an edge
+
+                }
+            }
+
         }
+
         catch (java.lang.IndexOutOfBoundsException e) {
             System.out.println("Outside prescribed range");
             return false;
@@ -47,11 +76,11 @@ public class Percolation {
     }
 
     public boolean percolates(){ //does the system percolate?
-        //TODO check bottom sites for a site that is open, if so call isFull
+        //Check bottom sites for a site that is open, if so call isFull
         int size = grid.length;
         for (int i = 0; i<size; i++) {
-            if (isOpen(size,i)){ //if bottom site is open
-                if(isFull(size,i)){ //if bottom site is full, this is more expensive so checking if its open first makes more sense
+            if (isOpen(size-1,i)){ //if bottom site is open
+                if(isFull(size-1,i)){ //if bottom site is full, this is more expensive so checking if its open first makes more sense
                     return true; //if bottom site is full it means it will percolate, no more sites need to be tested.
                 }
             }
@@ -60,9 +89,10 @@ public class Percolation {
     }
 
     public static void main(String args[]){
+        System.out.println("Percolation Started");
         Percolation p = new Percolation(20);
-        p.percolates();
-        System.out.println("hello world");
+        System.out.println("Percolates? " + p.percolates());
+
     }
 }
 
