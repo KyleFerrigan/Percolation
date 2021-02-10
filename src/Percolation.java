cc32ae6d590a7ff0abb783b7a42e0814646f2dbc
Kyle Ferrigan <kbferrigan@outlook.com>
2021-02-10 13:42:12 -0600

    Added basic functionality to Percolation.java

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,16 +1,20 @@
 public class Percolation {
-
+    boolean[][] grid;
     public Percolation(int N){ //create N-by-N grid, with all sites blocked
-
+        grid = new boolean[N][N];
+        for (int i = 0; i<N; i++){
+            for (int j=0; i<N; i++){
+                grid[i][j] = false; //False means blocked, all sites blocked with this command
+            }
+        }
     }
 
     public void open(int i, int j){ //open site (row i, column j) if it is not open already
-
+        grid[i][j] = true; //Opens site
     }
 
     public boolean isOpen(int i, int j){ //is site (row i, column j) open?
-
-        return false; //placeholder
+        return grid[i][j]; //returns bool if its open
     }
 
     public boolean isFull(int i, int j){ //is site (row i, column j) full?
