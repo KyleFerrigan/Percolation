e56ac8c9f8c8673b4da556dbb36064499cfc134f
Kyle Ferrigan <kbferrigan@outlook.com>
2021-02-09 19:50:36 -0600

    Fixed Framework so its runnable, but has no functionality yet

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,8 +1,26 @@
 public class Percolation {
-    public Percolation(int N)             // create N-by-N grid, with all sites blocked
-    public void open(int i, int j)        // open site (row i, column j) if it is not open already
-    public boolean isOpen(int i, int j)   // is site (row i, column j) open?
-    public boolean isFull(int i, int j)   // is site (row i, column j) full?
-    public boolean percolates()           // does the system percolate?
+
+    public Percolation(int N){ //create N-by-N grid, with all sites blocked
+
+    }
+
+    public void open(int i, int j){ //open site (row i, column j) if it is not open already
+
+    }
+
+    public boolean isOpen(int i, int j){ //is site (row i, column j) open?
+
+        return false; //placeholder
+    }
+
+    public boolean isFull(int i, int j){ //is site (row i, column j) full?
+
+        return false;//placeholder
+    }
+    
+    public boolean percolates(){ //does the system percolate?
+
+        return false; //placeholder
+    }
 }
 
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,8 +1,26 @@
 public class PercolationStats {
-    public PercolationStats(int N, int T)   // perform T independent experiments on an N-by-N grid
-    public double mean()                    // sample mean of percolation threshold
-    public double stddev()                  // sample standard deviation of percolation threshold
-    public double confidenceLow()           // low  endpoint of 95% confidence interval
-    public double confidenceHigh()          // high endpoint of 95% confidence interval
-}
 
+    public PercolationStats(int N, int T){ // perform T independent experiments on an N-by-N grid
+
+    }
+
+    public double mean(){ //sample mean of percolation threshold
+
+        return -1; //placeholder
+    }
+
+    public double stddev(){ //sample standard deviation of percolation threshold
+
+        return -1;//placeholder
+    }
+
+    public double confidenceLow(){ //low  endpoint of 95% confidence interval
+
+        return -1;//placeholder
+    }
+
+    public double confidenceHigh(){ //high endpoint of 95% confidence interval
+
+        return -1; //placeholder
+    }
+}
